3eecf0542907bf39c6f0a9ed38bb34a0cd67286c
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-08 13:54:50 -0400

    btn view more -> displaying more posts AND btn view less -> displaying less posts

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ function renderData(startIndex, postsLength) {
       </article>
      `;
   }
-  mainTag.innerHTML += articles;
+  mainTag.innerHTML = articles;
 }
 
 renderData(0, defaultNumOfPostsDisplay);
+displayPosts(defaultNumOfPostsDisplay);
 
-document.getElementById("btn-view").addEventListener("click", (e) => {
-  e.preventDefault();
-  const postsDisplayed = defaultNumOfPostsDisplay;
-  renderData(postsDisplayed, posts.length);
-});
+function displayPosts(postsDisplayed) {
+  const btnView = document.getElementById("btn-view");
+  let clickCount = 0;
+  function handleClick() {
+    btnView.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (clickCount === 1 && btnView.textContent === "View Less") {
+        renderData(0, postsDisplayed);
+        btnView.textContent = "View More";
+        clickCount--;
+      } else if (clickCount === 0 && btnView.textContent === "View More") {
+        renderData(0, posts.length);
+        btnView.textContent = "View Less";
+        clickCount++;
+      }
+    });
+  }
+  return handleClick();
+}
