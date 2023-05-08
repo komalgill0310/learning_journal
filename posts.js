073fb4a5a8c2971fa218bf986111c311df61afb8
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-08 09:27:07 -0400

    styling for hero section is done

--- posts.js
+++ posts.js
@@ -4,7 +4,7 @@ export const posts = [
     img: "images/hero_images/hero-post-background-image.avif",
     alt: "image information",
     date: "Apr22,2023",
-    title: "Hero Blog",
+    title: "My new journey as a bootcamp student.",
     body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id minima magnam blanditiis saepe consequatur reiciendis, numquam magni fuga. Temporibus exercitationem deserunt nobis ratione, iure aut quasi dolor modi ea harum!",
   },
   {
