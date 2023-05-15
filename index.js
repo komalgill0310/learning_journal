7b866b58148a1d57fd1410324166b6bde213b782
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-15 17:01:31 -0400

    update hero post's html and style when user click on the about me nav link

--- index.js
+++ index.js
@@ -1,53 +1,27 @@
 import { posts } from "/posts.js";
 
 const defaultNumOfPostsDisplay = 4;
-
+const heroIndex = 0;
 const mainTag = document.querySelector(".main");
+
+renderData(0, defaultNumOfPostsDisplay);
+displayPosts(defaultNumOfPostsDisplay);
+handleAboutMeClick();
+
 function renderData(startIndex, postsLength) {
   let articles = "";
   for (let i = startIndex; i < postsLength; i++) {
     articles += createPost(i);
   }
   mainTag.innerHTML += articles;
+  // updateHeroArticle();
   heroImageClick();
-  // aboutMe();
 }
-
-function createPost(index) {
-  const post = `
-  <article class=${posts[index].id}>
-    <img class="article-img" src=${posts[index].img} alt=${posts[index].alt}/>
-    <p class="article-date">${posts[index].date}</p>
-    <h2 class="article-title">${posts[index].title}</h2>
-    <p class="article-body">${posts[index].body}</p>
-  </article>
- `;
-  return post;
-}
-
-function heroImageClick() {
-  const heroIndex = 0;
-  const articleHero = document.querySelector(`.${posts[heroIndex].id}`);
-  articleHero.addEventListener("click", (e) => {
-    e.preventDefault();
-    articleHero.innerHTML += `
-      <h4 class="article-hero-sub-heading-one">How I stay committed to learning</h4>
-      <p class="article-hero-sub-heading-body-one">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae enim obcaecati numquam quidem vero praesentium, hic nulla ab sed iusto culpa voluptatibus molestias debitis? Accusamus reiciendis vel labore deserunt!</p>
-      <h4 class="article-hero-sub-heading-two">How I got started</h4>
-      <p class="article-hero-sub-heading-body-two">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae enim obcaecati numquam quidem vero praesentium, hic nulla ab sed iusto culpa voluptatibus molestias debitis? Accusamus reiciendis vel labore deserunt!</p>
-      <h4 class="recent-posts-heading">Recent Posts</h4>
-    `;
-    articleHero.classList.add("active");
-  });
-}
-
-renderData(0, defaultNumOfPostsDisplay);
-displayPosts(defaultNumOfPostsDisplay);
 
 function displayPosts(postsDisplayed) {
   const btnView = document.getElementById("btn-view");
   let clickCount = 0;
-  function handleClick() {
+  function handleBtnViewClick() {
     btnView.addEventListener("click", (e) => {
       e.preventDefault();
       if (clickCount === 1 && btnView.textContent === "View Less") {
@@ -62,13 +36,71 @@ function displayPosts(postsDisplayed) {
       }
     });
   }
-  return handleClick();
+  return handleBtnViewClick();
 }
 
-// function aboutMe() {
-const aboutMe = document.querySelector(".about-me");
-aboutMe.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("I am about Me!");
-});
+function createPost(index) {
+  const post = `
+  <article class=${posts[index].id}>
+    <img class="article-img" src=${posts[index].img} alt=${posts[index].alt}/>
+    <p class="article-date">${posts[index].date}</p>
+    <h2 class="article-title">${posts[index].title}</h2>
+    <p class="article-body">${posts[index].body}</p>
+  </article>
+ `;
+  return post;
+}
+
+// function updateHeroArticle() {
+//   const isHeroImageSrcIncluded = getElement(heroIndex)
+//     .querySelector(".article-img")
+//     .src.includes(`${posts[heroIndex].img}`);
+//   if (isHeroImageSrcIncluded) {
+//     heroImageClick();
+//     console.log(isHeroImageSrcIncluded);
+//   }
 // }
+
+function heroImageClick() {
+  getElement(heroIndex).addEventListener("click", () => {
+    if (
+      getElement(heroIndex)
+        .querySelector(".article-img")
+        .src.includes(`${posts[heroIndex].img}`)
+    ) {
+      getElement(heroIndex).innerHTML += updateHeroPost();
+      getElement(heroIndex).classList.add("active");
+      console.log("class active");
+    }
+  });
+}
+
+function handleAboutMeClick() {
+  const aboutMe = document.querySelector(".about-me");
+  const heroImg = getElement(heroIndex).querySelector(".article-img");
+  const heroTitle = getElement(heroIndex).querySelector(".article-title");
+  const heroDate = getElement(heroIndex).querySelector(".article-date");
+  aboutMe.addEventListener("click", () => {
+    heroDate.textContent = "";
+    heroImg.src = "images/hero_images/profile-pic.jpg";
+    heroTitle.textContent =
+      "Hello there! My name is Komal and welcome to my learning journal.";
+    getElement(heroIndex).innerHTML += updateHeroPost();
+    getElement(heroIndex).classList.remove("active");
+    getElement(heroIndex).classList.add("about-me");
+  });
+}
+
+function updateHeroPost() {
+  return `
+  <h4 class="article-hero-sub-heading-one">How I stay committed to learning</h4>
+  <p class="article-hero-sub-heading-body-one">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae enim obcaecati numquam quidem vero praesentium, hic nulla ab sed iusto culpa voluptatibus molestias debitis? Accusamus reiciendis vel labore deserunt!</p>
+  <h4 class="article-hero-sub-heading-two">How I got started</h4>
+  <p class="article-hero-sub-heading-body-two">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae enim obcaecati numquam quidem vero praesentium, hic nulla ab sed iusto culpa voluptatibus molestias debitis? Accusamus reiciendis vel labore deserunt!</p>
+  <h4 class="recent-posts-heading">Recent Posts</h4>
+`;
+}
+
+function getElement(index) {
+  return document.querySelector(`.${posts[index].id}`);
+}
